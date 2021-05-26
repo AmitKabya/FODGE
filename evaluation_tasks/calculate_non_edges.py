eb5be70a48aac140462364d323f4a23d085db3ba
unknownuser13570 <78530623+unknownuser13570@users.noreply.github.com>
2021-05-26 17:11:23 +0300

    Update calculate_non_edges.py

--- evaluation_tasks/calculate_non_edges.py
+++ evaluation_tasks/calculate_non_edges.py
@@ -41,14 +41,17 @@ def load_graph(func, t):
 
 
 def create_non_edges_file(name, path, func):
+    print("First creating non edges file")
     dict_snapshots, dict_weights = load_graph(func, (name, path))
+    print("Starting create graphs")
     g_list = create_graphs(dict_snapshots)
     my_list = []
+    print("Starting find non edges for each time")
     for i in range(len(g_list)):
         g = g_list[i]
         e = g.number_of_edges()
         n = g.number_of_nodes()
-        print(n, e)
+        print(i, n, e)
         nodes = list(g.nodes())
         # could be very big so change 0.5 to smaller values if needed.
         indexes = random.sample(range(0, len(nodes)), int(len(nodes) * 0.5))
@@ -56,19 +59,20 @@ def create_non_edges_file(name, path, func):
         for j in indexes:
             new_nodes.append(nodes[j])
         missing = [pair for pair in IT.combinations(new_nodes, 2) if not g.has_edge(*pair)]
-        print(i)
-        print(len(missing))
+        print(i, len(missing))
         for pair in missing:
             my_list.append((i, pair[0], pair[1]))
 
-        # indexes = random.sample(range(0, len(my_list)), int(len(my_list)))
-        # new_list = []
-        # for l in indexes:
-        #   new_list.append(my_list[l])
-        csvfile = open('evaluation_tasks/non_edges_{}.csv'.format(name), 'w', newline='')
-        obj = csv.writer(csvfile)
-        obj.writerows(my_list)
-        csvfile.close()
+    # indexes = random.sample(range(0, len(my_list)), int(len(my_list)))
+    # new_list = []
+    # for l in indexes:
+    #   new_list.append(my_list[l])
+    print("Almost done, writing the csv file!")
+    csvfile = open('evaluation_tasks/non_edges_{}.csv'.format(name), 'w', newline='')
+    obj = csv.writer(csvfile)
+    obj.writerows(my_list)
+    csvfile.close()
+    print("Non edges file is ready!")
 
 
 # name_ = "facebook_friendships"
